bebf6b470a1b30630c9fc24d537e19eb06e78a75
Manuel Rosales <marosales.24@est.ucab.edu.ve>
2025-05-18 23:45:15 -0400

    Version 2.0

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 #include <vector>
-#include "cards.h"
+#include "players.h"
 
 using namespace std;
 
 
 int main(){
     Cards* myDeck = nullptr;
+    Player* players = nullptr;
     int numCards = 0;
-
+    string name1;
+    string name2;
     myDeck = initializeDeck(numCards);
-    std::cout << "Initial deck:" << std::endl;
-    displayDeck(myDeck, numCards);
-
     myDeck = shuffleDeck(myDeck, numCards);
-    std::cout << "\nShuffled deck:" << std::endl;
     displayDeck(myDeck, numCards);
+    cout<<"Ingrese el nombre del jugador 1"<<endl;
+    cin>>name1;
+    createPlayer(players, name1, 1);
+    cout<<"Ingrese el nombre del jugador 2"<<endl;
+    cin>>name2;
+    createPlayer(players, name2, 2);
+    displayplayers(players);
+    repartirMazo(myDeck, players);
+    
 
 }
